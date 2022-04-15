b518e06ada33a917f76558c2a438c12982254e03
성진옥 <wlsdhr0831@naver.com>
2022-04-16 03:44:39 +0900

    feat: url path에 따라 라우팅 될 페이지 예시 추가

---
+++ src/pages/MainMap/MainMap.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainMap = () => {
+  return <div>메인 화면 - 바다</div>;
+};
+
+export default MainMap;
---
+++ src/pages/MainSea/MainSea.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainSea = () => {
+  return <div>메인 화면 - 바다</div>;
+};
+
+export default MainSea;
