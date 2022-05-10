dfbc1b634e2ed08c33cb23ce678d5ecd415b1edc
Hyelme <jhrim0810@naver.com>
2022-05-11 08:31:15 +0900

    Feat: 로그인 페이지 구축

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
+import { InputTxt } from "components/common/InputTxt"
+import { Button } from "components/common/Button"
+
+import "./signIn.css"
+
 const SignIn = () => {
-  return <div>로그인</div>;
+  return (
+    <div className="signIn-wrap">
+      <div className="logo-wrap">
+        <div className="logo">
+          <p className="logo-txt">
+           logo
+          </p>
+        </div>
+      </div>
+      <div className="input-comp-wrap">
+        <InputTxt type="text" size="medium" placeholder="닉네임" />
+        <InputTxt type="password" size="medium" placeholder="비밀번호" />
+
+        <Button size="medium" label="비밀번호 찾기" />&nbsp; | &nbsp;
+        <Button size="medium" label="회원가입" />
+      </div>
+      <div className="signIn-btn-wrap">
+        <Button size="bottom" label="로그인" />
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
