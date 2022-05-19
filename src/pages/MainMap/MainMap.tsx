33225697ae7d08613898097162bcb30c16c2d6a7
성진옥 <wlsdhr0831@naver.com>
2022-05-19 19:21:51 +0900

    Feat: 페이지 이동 버튼 추가

--- src/pages/MainMap/MainMap.tsx
+++ src/pages/MainMap/MainMap.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { UrlPaths } from "../../routes";
 
 const MainMap = () => {
-  return <div>메인 화면 - 바다</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          navigate("/main/sea");
+        }}
+      >
+        바다로 가기
+      </button>
+      메인 화면 - 지도
+    </div>
+  );
 };
 
 export default MainMap;
--- src/pages/MainSea/MainSea.tsx
+++ src/pages/MainSea/MainSea.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { UrlPaths } from "../../routes";
 
 const MainSea = () => {
-  return <div>메인 화면 - 바다</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          navigate("/main/map");
+        }}
+      >
+        지도로 가기
+      </button>
+      메인 화면 - 바다
+    </div>
+  );
 };
 
 export default MainSea;
