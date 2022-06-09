b32bee6e634c7b441a408ec369d3fcc2d9bf7ca2
성진옥 <wlsdhr0831@naver.com>
2022-06-09 20:46:31 +0900

    Feat: 바다화면 종류 추가

--- src/pages/MainSea/MainSea.view.tsx
+++ src/pages/MainSea/MainSea.view.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from "react-router-dom";
+
 export interface Props {
   unknownCapsule: string;
   hasCapsule: boolean;
@@ -9,13 +11,27 @@ const MainSeaView = ({
   hasCapsule,
   showDescription,
 }: Props) => {
+  const navigate = useNavigate();
+
   return (
     <>
       <button>설정</button>
       {hasCapsule ? (
-        <button>지도로 가기</button>
+        <button
+          onClick={() => {
+            navigate("/main/map");
+          }}
+        >
+          지도로 가기
+        </button>
       ) : (
-        <button>캡술 만들러 가기</button>
+        <button
+          onClick={() => {
+            navigate("/capsule/design");
+          }}
+        >
+          캡술 만들러 가기
+        </button>
       )}
       {showDescription && <div>바다 화면에서 캡슐을 다시 볼 수 있어요!</div>}
       <div>{unknownCapsule && "익명의 캡슐이 있네요. 보시겠습니까?"}</div>
