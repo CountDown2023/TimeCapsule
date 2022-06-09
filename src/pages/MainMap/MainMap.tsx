0d07c315c2a91d0322bc30264b3735b9f7f4dcb2
성진옥 <wlsdhr0831@naver.com>
2022-06-09 20:48:53 +0900

    Feat: 지도화면 종류 추가

--- src/pages/MainMap/MainMap.tsx
+++ src/pages/MainMap/MainMap.tsx
@@ -1,28 +1,22 @@
 import React from "react";
 import { useQuery } from "react-query";
-import { useNavigate } from "react-router-dom";
 import { getCapsuleInfo } from "../../api/capsule";
 import MainMapView from "./MainMap.view";
 
 const MainMap = () => {
-  const navigate = useNavigate();
-
   const { isLoading, data, isError } = useQuery("getCapsuleInfo", () =>
     getCapsuleInfo()
   );
 
-  return (
-    <div>
-      <button
-        onClick={() => {
-          navigate("/main/sea");
-        }}
-      >
-        바다로 가기
-      </button>
-      <MainMapView days={data ? "" : ""} />
-    </div>
-  );
+  if (isLoading) {
+    return <>로딩중입니다...</>;
+  }
+
+  if (isError) {
+    return <>에러가 발생했습니다. 다시 시도해주세요.</>;
+  }
+
+  return <MainMapView days={data ? "" : ""} />;
 };
 
 export default MainMap;
--- src/pages/MainMap/MainMap.view.tsx
+++ src/pages/MainMap/MainMap.view.tsx
@@ -1,10 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 export interface Props {
   days: string;
 }
 
 const MainMapView = ({ days }: Props) => {
+  const navigate = useNavigate();
+
   return (
     <>
+      <button
+        onClick={() => {
+          navigate("/main/sea");
+        }}
+      >
+        바다로 가기
+      </button>
       <div>{days}일 째 여행 중</div>
     </>
   );
