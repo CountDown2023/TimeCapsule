3ada8ebea84537cf1c5350305798e0f0a28e8469
eeapbh <eeapbh@gmail.com>
2022-08-08 20:19:49 +0900

    Refac: 예전 폴더 Design,DesignNav, DesignSettings 삭제

--- src/pages/DesignSettings/TabContent.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const TabContent = (props: any) => {
-  if (props.tab === 0) return <div>0</div>;
-  else if (props.tab === 1) return <div>1</div>;
-  else return <div>2</div>;
-};
-
-export default TabContent;
