169dd158472337321aa05d5aace33a34a179994a
성진옥 <wlsdhr0831@naver.com>
2022-09-20 00:41:18 +0900

    feat: index 파일 추가

---
+++ src/assets/images/letterPaper/index.tsx
@@ -0,0 +1,8 @@
+export { default as CirclePaper } from "./circle.png";
+export { default as CloudPaper } from "./cloud.png";
+export { default as CrossPaper } from "./cross.png";
+export { default as FlowerPaper } from "./flower.png";
+export { default as GrassPaper } from "./grass.png";
+export { default as HeartPaper } from "./heart.png";
+export { default as NotePaper } from "./note.png";
+export { default as StarPaper } from "./star.png";
---
+++ src/assets/images/palette/index.tsx
@@ -0,0 +1,2 @@
+export { default as Color } from "./color.png";
+export { default as Light } from "./light.png";
