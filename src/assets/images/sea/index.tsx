6dee0e45253ad3fcfb063c050aa94d9d63ee9f78
성진옥 <wlsdhr0831@naver.com>
2022-10-24 00:51:06 +0900

    feat: 이미지 추가

---
+++ src/assets/images/sea/index.tsx
@@ -0,0 +1,7 @@
+export { default as BubbleCenter } from "./bubble_center.png";
+export { default as BubbleFront } from "./bubble_front.png";
+export { default as SkyDay } from "./sky_day.png";
+export { default as SkyNight } from "./sky_night.png";
+export { default as SunMoon } from "./sun_moon.png";
+export { default as WaterDay } from "./water_day.png";
+export { default as WaterNight } from "./water_night.png";
