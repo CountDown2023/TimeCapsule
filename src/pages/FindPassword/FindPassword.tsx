dd30ece11b8c736026321a23b4ddb3ea37e6a41a
Hyelme <jhrim0810@naver.com>
2022-11-05 15:46:25 +0900

    feat: 비밀번호 찾기 페이지 생성

--- src/pages/FindPassword/FindPassword.tsx
+++ src/pages/FindPassword/FindPassword.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { InputText } from "components/common/inputText/InputText";
+import { InputForm } from "pages/common/InputForm/InputForm";
 
-const FindPassword = () => {
-  return <div>비밀번호 찾기</div>;
+import { useNavigate } from 'react-router-dom';
+
+import './findPassword.css';
+
+export interface FindPwdProps {
+  isFailed?: boolean;
+}
+
+const FindPassword = ({
+  isFailed = false
+}: FindPwdProps) => {
+
+  const navigate = useNavigate();
+
+  const [modeValue, setModeValue] = useState<"normal" | "focus" | "warning">(isFailed ? "warning" : "normal")
+  const [stateValues, setStateValues] = useState({
+    nickname: "",
+    email: ""
+  });
+
+  const clickSubmit = () => {
+    console.log("비밀번호 찾기 테스트");
+    console.log("nickname : ", stateValues.nickname);
+    console.log("email : ", stateValues.email);
+
+    //TODO: 비밀번호 확인 API 연동 로직 구현
+
+  }
+
+  return (
+    <InputForm 
+      showBackBtn={true} 
+      onClickBackBtn={true}
+      submitButtonText={"비밀번호 찾기"}
+      disabledSubmitButton={true}
+      onSubmit={clickSubmit}
+    >
+      <div className='signUp-wrap'>
+        <div className='input-comp-wrap' onFocus={() => setModeValue('focus')} onBlur={() => setModeValue('normal')}>
+          <InputText
+            type='text'
+            inputId="findNickname"
+            mode={modeValue}
+            widthSize="medium"
+            placeholder='닉네임'
+            onChangeValue={(value: string) =>
+              setStateValues({ ...stateValues, nickname: value})
+            }
+          />
+          <InputText
+            type='text'
+            inputId="findEmail"
+            mode={modeValue}
+            widthSize="medium"
+            placeholder='안내 받을 이메일'
+            onChangeValue={(value: string) =>
+              setStateValues({ ...stateValues, email: value })
+            }
+          />
+        </div>
+      </div>
+    </InputForm>
+  );
 };
 
 export default FindPassword;
