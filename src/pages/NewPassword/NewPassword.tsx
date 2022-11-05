8356314e3d7c2a4ee5ca18e775badd6fb6bf743e
Hyelme <jhrim0810@naver.com>
2022-11-05 15:31:12 +0900

    feat: 비밀번호 수정 페이지 생성

--- src/pages/NewPassword/NewPassword.tsx
+++ src/pages/NewPassword/NewPassword.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { InputText } from "components/common/inputText/InputText";
+import { InputForm } from "pages/common/InputForm/InputForm";
 
-const NewPassword = () => {
-  return <div>비밀번호 설정</div>;
+import { useNavigate } from 'react-router-dom';
+
+import './newPassword.css';
+
+export interface NewPwdProps {
+  isFailed?: boolean;
+}
+
+const NewPassword = ({
+  isFailed = false
+}:NewPwdProps) => {
+
+  const navigate = useNavigate();
+
+  const [modeValue, setModeValue] = useState<"normal" | "focus" | "warning">(isFailed ? "warning" : "normal")
+  const [stateValues, setStateValues] = useState({
+    newPwd: "",
+    newPwdConfirm: ""
+  });
+
+  const clickSubmit = () => {
+    console.log("비밀번호 수정 테스트");
+    console.log("newPwd : ", stateValues.newPwd);
+    console.log("newPwdConfirm : ", stateValues.newPwdConfirm);
+
+    //TODO: 비밀번호 확인 API 연동 로직 구현
+
+  }
+
+  return (
+    <InputForm 
+      showBackBtn={true} 
+      onClickBackBtn={true}
+      submitButtonText={"비밀번호 수정"}
+      disabledSubmitButton={true}
+      onSubmit={clickSubmit}
+    >
+      <div className='newPwd-wrap'>
+        <div className='input-comp-wrap' onFocus={() => setModeValue('focus')} onBlur={() => setModeValue('normal')}>
+          <InputText
+            type='password'
+            inputId="newPwd"
+            mode={(modeValue == 'warning')?'normal' : modeValue}
+            widthSize="medium"
+            placeholder='새 비밀번호'
+            onChangeValue={(value: string) =>
+              setStateValues({ ...stateValues, newPwd: value})
+            }
+          />
+          <InputText
+            type='password'
+            inputId="newPwdConfirm"
+            mode={modeValue}
+            widthSize="medium"
+            placeholder='새 비밀번호 확인'
+            onChangeValue={(value: string) =>
+              setStateValues({ ...stateValues, newPwdConfirm: value})
+            }
+          />
+        </div>
+      </div>
+    </InputForm>
+  );
 };
 
 export default NewPassword;
