143de0c86307c443cafa54082543347e77d33f09
eeapbh <eeapbh@gmail.com>
2022-11-07 13:18:29 +0900

    Feat: Capsule Input 메인으로 가기 버튼생성, 클릭시 dialog

--- src/pages/CapsuleDesign/components/CapsuleInput/CapsuleInput.tsx
+++ src/pages/CapsuleDesign/components/CapsuleInput/CapsuleInput.tsx
@@ -31,7 +31,7 @@ const CapsuleInput = () => {
         onClickOkButton={() => setIsShowSuccessDialog(false)}
       />
       <Dialog
-        className="saveDialog"
+        className="goMainDialog"
         isShow={isShowGoMainDialog}
         title={"중간에 화면을 나가시면 저장이 되지 않아요. 괜찮으신가요?"}
         okButton="네 괜찮습니다."
