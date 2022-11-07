bb35216049a352102a6f573c73455d83befffd5a
eeapbh <eeapbh@gmail.com>
2022-11-07 14:04:07 +0900

    Refac: dialog title ""수정

--- src/pages/CapsuleDesign/components/CapsuleInput/CapsuleInput.tsx
+++ src/pages/CapsuleDesign/components/CapsuleInput/CapsuleInput.tsx
@@ -26,14 +26,14 @@ const CapsuleInput = () => {
       <Dialog
         className="saveDialog"
         isShow={isShowSuccessDialog}
-        title={<>캡슐 생성이 완료되었습니다.</>}
+        title={"캡슐 생성이 완료되었습니다."}
         okButton="확인"
         onClickOkButton={() => setIsShowSuccessDialog(false)}
       />
       <Dialog
         className="saveDialog"
         isShow={isShowGoMainDialog}
-        title={<>중간에 화면을 나가시면 저장이 되지 않아요. 괜찮으신가요?</>}
+        title={"중간에 화면을 나가시면 저장이 되지 않아요. 괜찮으신가요?"}
         okButton="네 괜찮습니다."
         cancleButton="여기 남아있을래요!"
         onClickCancleButton={() => setIsShowGoMainDialog(false)}
