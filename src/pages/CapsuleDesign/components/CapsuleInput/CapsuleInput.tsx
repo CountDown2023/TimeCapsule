10c27aab63d0e9ee1b62133f6d0c8fd2ca50529b
성진옥 <wlsdhr0831@naver.com>
2022-11-22 21:34:33 +0900

    feat: module scss 적용

--- src/pages/CapsuleDesign/components/CapsuleInput/CapsuleInput.tsx
+++ src/pages/CapsuleDesign/components/CapsuleInput/CapsuleInput.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./CapsuleInput.css";
+import styles from "./CapsuleInput.module.scss";
 import Dialog from "../../../../components/common/dialog";
 
 const CapsuleInput = () => {
@@ -9,9 +9,9 @@ const CapsuleInput = () => {
   const [saveComplete, setIsSaveComplete] = useState<boolean>(false);
 
   return (
-    <div className="inputWrapper">
+    <div className={styles.container}>
       <button
-        className="capsuleInputBtn"
+        className={styles.submitButton}
         onClick={() => {
           setIsShowSuccessDialog(true);
           setIsSaveComplete(true);
@@ -20,19 +20,22 @@ const CapsuleInput = () => {
         작성완료
       </button>
 
-      <button className="goMainBtn" onClick={() => setIsShowGoMainDialog(true)}>
+      <button
+        className={styles.backButton}
+        onClick={() => setIsShowGoMainDialog(true)}
+      >
         메인으로 가기
       </button>
 
       <Dialog
-        className="saveDialog"
+        className={styles.dialog}
         isShow={isShowSuccessDialog}
         title={"캡슐 생성이 완료되었습니다."}
         okButton="확인"
         onClickOkButton={() => setIsShowSuccessDialog(false)}
       />
       <Dialog
-        className="saveDialog"
+        className={styles.dialog}
         isShow={isShowGoMainDialog}
         title={"중간에 화면을 나가시면 저장이 되지 않아요. 괜찮으신가요?"}
         okButton="네 괜찮습니다."
@@ -40,35 +43,38 @@ const CapsuleInput = () => {
         onClickCancleButton={() => setIsShowGoMainDialog(false)}
         onClickOkButton={() => setIsShowGoMainDialog(true)}
       />
-      <div className="inputWrapper2">
+      <div className={styles.form}>
         <input
-          className="capsuleName"
+          className={styles.name}
           type="text"
           placeholder="캡슐 이름을 입력하세요."
         />
-        <ul className="capsuleInput">
-          <div className="capsuleDescription">이루고싶은 목표를 적어보세요</div>
-          <li className="capsuleLi">
-            <input className="capsuleLiInput" type="text" />
+        <ul className={styles.capsuleInput}>
+          <div className={styles.description}>이루고싶은 목표를 적어보세요</div>
+          <li className={styles.content}>
+            <input type="text" />
           </li>
-          <li className="capsuleLi">
-            <input className="capsuleLiInput" type="text" />
+          <li className={styles.content}>
+            <input type="text" />
           </li>
-          <li className="capsuleLi">
-            <input className="capsuleLiInput" type="text" />
+          <li className={styles.content}>
+            <input type="text" />
           </li>
-          <li className="capsuleLi">
-            <input className="capsuleLiInput" type="text" />
+          <li className={styles.content}>
+            <input type="text" />
           </li>
-          <li className="capsuleLi">
-            <input className="capsuleLiInput" type="text" />
+          <li className={styles.content}>
+            <input type="text" />
           </li>
-          <li className="capsuleLi">
-            <input className="capsuleLiInput" type="text" />
+          <li className={styles.content}>
+            <input type="text" />
           </li>
         </ul>
 
-        <textarea className="toMe" placeholder="12월 31일의 나에게 한마디" />
+        <textarea
+          className={styles.mySelf}
+          placeholder="12월 31일의 나에게 한마디"
+        />
       </div>
     </div>
   );
