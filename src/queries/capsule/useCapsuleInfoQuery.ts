a989f4bf055cf54ff68b469b70206fb9c53aa680
성진옥 <wlsdhr0831@naver.com>
2022-12-04 13:39:05 +0900

    feat: 캡슐 관련 API 요청 쿼리 제작

---
+++ src/queries/capsule/useCapsuleInfoQuery.ts
@@ -0,0 +1,18 @@
+import { getCapsuleInfo, GetCapsuleInfoResponse } from "api/capsule";
+import { useQuery, UseQueryOptions } from "react-query";
+
+const useCapsuleInfoQuery = (
+  options?: UseQueryOptions<GetCapsuleInfoResponse>
+) => {
+  const response = useQuery<GetCapsuleInfoResponse>(
+    "getCapsuleInfo",
+    () => getCapsuleInfo(),
+    {
+      ...options,
+    }
+  );
+
+  return response;
+};
+
+export default useCapsuleInfoQuery;
---
+++ src/queries/capsule/useUnknownCapsuleQuery.ts
@@ -0,0 +1,18 @@
+import { hasUnknownCapsule, HasUnknownCapsuleResponse } from "api/capsule";
+import { useQuery, UseQueryOptions } from "react-query";
+
+const useUnknownCapsuleQuery = (
+  options?: UseQueryOptions<HasUnknownCapsuleResponse>
+) => {
+  const response = useQuery<HasUnknownCapsuleResponse>(
+    "hasUnknownCapsule",
+    () => hasUnknownCapsule(),
+    {
+      ...options,
+    }
+  );
+
+  return response;
+};
+
+export default useUnknownCapsuleQuery;
