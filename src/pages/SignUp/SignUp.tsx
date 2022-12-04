f464c45c3bf4e5242d92c49ee510581368cd4fa8
Hyelme <jhrim0810@naver.com>
2022-12-04 16:29:40 +0900

    feat: 회원가입 api 연동

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,4 +1,6 @@
-import { useNavigate } from 'react-router-dom';
+import { SignUpRequestData } from 'api/user';
+import { useState } from 'react';
+import useSignUp from './hook/useSignUp';
 
 import SignUpView from './SignUp.view';
 
@@ -6,17 +8,39 @@ export type SignUpForm = {nickname: string, password: string, passwordConfirm: s
 
 const SignUp = () => {
 
+  const [ reqSignUpData, setReqSignUpData ] = useState<SignUpRequestData>({
+    nickname: "",
+    password: ""
+  });
+
+  const { isLoading, isError, isJoined, mutate } = useSignUp(reqSignUpData);
+
   const clickSubmit = (data: SignUpForm) => {
     console.log("회원가입 테스트");
     console.log("email : ", data.nickname);
     console.log("pwd : ", data.password);
     console.log("pwd confirm : ", data.passwordConfirm);
 
-    //TODO: 회원가입 API 연동 로직 구현
+    //TODO: 비밀번호/비밀번호 확인 validation 체크 로직 구현
+    const requestData:SignUpRequestData = {
+      nickname: data.nickname,
+      password: data.password
+    };
+
+    setReqSignUpData(requestData);
+    mutate();
+  }
+
+  if(isLoading) {
+    return <>Loading....</>
+  }
+
+  if(isError) {
+    return <>에러가 발생했습니다. 다시 시도해주세요.</>
   }
 
   //TODO: isJoined 값 변경 필요
-  return <SignUpView isJoined={true} clickSubmit={clickSubmit}/>;
+  return <SignUpView isJoined={isJoined} clickSubmit={clickSubmit}/>;
 };
 
 export default SignUp;
