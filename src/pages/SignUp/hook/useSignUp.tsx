e7d461e70cf3c3a031c84c1248b18ce98e461a85
Hyelme <jhrim0810@naver.com>
2022-12-04 16:29:19 +0900

    feat: signUp hook 생성

---
+++ src/pages/SignUp/hook/useSignUp.tsx
@@ -0,0 +1,23 @@
+import { SignUpRequestData } from "api/user";
+import { HTTP_STATUS } from "configs/axios";
+import useSignUpQuery from "queries/user/useSignUpQuery";
+
+const useSignUp = (data: SignUpRequestData) => {
+  const {
+    isLoading: isSignUpLoading,
+    data: signUp,
+    isError: isSignUpError,
+    mutate
+  } = useSignUpQuery(data);
+
+  return {
+    isLoading: isSignUpLoading,
+    data: signUp,
+    isError: isSignUpError,
+    mutate,
+    //TODO: isJoined 어케 판단할지 수정 필요
+    isJoined: signUp?.code === HTTP_STATUS.BAD_REQUEST ? false : true
+  };
+};
+
+export default useSignUp;
