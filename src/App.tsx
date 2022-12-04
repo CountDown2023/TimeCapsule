4cf2cad06967081d06fcb4bb4727b95e211afde2
성진옥 <wlsdhr0831@naver.com>
2022-12-04 14:59:24 +0900

    feat: css -> scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import MainSea from "./pages/MainSea";
 import { UrlPaths } from "./routes";
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./static/fonts/font.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { initAxios } from "configs/axios";
 
