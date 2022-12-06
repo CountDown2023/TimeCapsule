a363fcc634b0f4855fa6038f4cb587bf5f3b771c
eeapbh <eeapbh@gmail.com>
2022-12-06 16:21:42 +0900

    Test: ec2 nginx실행 테스트

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>찾을 수 없는 페이지 입니다. 봉봉 테스트중다시</div>;
+  return <div>찾을 수 없는 페이지 입니다. 봉봉 테스트중ㄱ</div>;
 };
 
 export default NotFound;
