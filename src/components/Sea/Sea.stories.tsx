fcc8847152527cd6cd986fd1e4687e288efea203
성진옥 <wlsdhr0831@naver.com>
2022-12-18 14:06:28 +0900

    style: 바다 스토리북 코드 정리

--- src/components/Sea/Sea.stories.tsx
+++ src/components/Sea/Sea.stories.tsx
@@ -4,13 +4,10 @@ import SeaNight from "./SeaNight";
 
 export default {
   title: "Main/components/sea",
-  component: SeaDay,
 } as Meta;
 
 const Template1: Story = (args) => <SeaDay {...args} />;
-
 export const 낮 = Template1.bind({});
 
 const Template2: Story = (args) => <SeaNight {...args} />;
-
 export const 밤 = Template2.bind({});
