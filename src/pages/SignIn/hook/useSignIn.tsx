db8b5c0c3904b56ff0cb92565203bb91202b3532
Hyelme <jhrim0810@naver.com>
2022-12-18 18:27:10 +0900

    feat: 로그인 API 적용

---
+++ src/pages/SignIn/hook/useSignIn.tsx
@@ -0,0 +1,22 @@
+import { SignInBody } from "api/user";
+// import useSignInQuery from "queries/user/useSignInQuery";
+
+// const useSignIn = (data: SignInBody) => {
+//   const {
+//     isLoading: isSignInLoading,
+//     data: signIn,
+//     isError: isSignInError,
+//     mutate
+//   } = useSignInQuery(data);
+
+//   return {
+//     isLoading: isSignInLoading,
+//     data: signIn,
+//     isError: isSignInError,
+//     mutate,
+//     //TODO: isJoined 어케 판단할지 수정 필요
+//     isLoggedIn: isSignInError ? true : false
+//   };
+// };
+
+// export default useSignIn;
