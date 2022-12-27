f5d7f541c8948ab8da8ec7399497f98eed9a4f71
성진옥 <wlsdhr0831@naver.com>
2022-12-28 06:02:40 +0900

    fix: 빌드 에러 수정

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
 import { initAxios } from "./configs/axios";
 
 initAxios();
