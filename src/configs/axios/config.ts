9431c10b3d6962fcf666138f57fb75e7ebc41df6
성진옥 <wlsdhr0831@naver.com>
2022-12-28 08:17:37 +0900

    feat: 에러 처리 로직 수정

--- src/configs/axios/config.ts
+++ src/configs/axios/config.ts
@@ -34,21 +34,7 @@ const requestOnFullFilled = async (
 const responseOnFullFilled = async (
   response: AxiosResponse
 ): Promise<AxiosResponse> => {
-  // TODO: 토큰 새로 발급이 필요한 경우 처리 로직 케이스 추가
-  // 서버에서 내려오는 응답값 확인 후 수정 예정
-  // if (response.data === "SUCCESS") {
-    // return { ...response, data: response.data };
-  // } else {
-  //   throw new Error();
-    // throw new AxiosError(
-    //   response.data.error.message,
-    //   "", // TODO: res.data.error.code를 가지고 AxiosErrorCode로 설정
-    //   response.config,
-    //   response.request,
-    //   response
-    // );
-  // }
-
+  console.log("API SUCCESS", response);
   return response;
 };
 
@@ -60,42 +46,28 @@ const responseOnFullFilled = async (
  * GlobalErrorBoundary에서 처리할 에러 발생
  */
 const responseOnRejected = async (
-  error: AxiosError
+  error: AxiosError<ErrorResponse>
 ): Promise<AxiosError<ErrorResponse>> => {
-  return new Promise((_, reject) => {
-    const { code, response } = error;
-    const data: ErrorResponse = response?.data as unknown as ErrorResponse;
+  console.log("API ERROR", error);
 
-    if (code) {
-      if(data !== null && data.code !== undefined) reject(data); 
-      switch (code) {
+  return new Promise((_, reject) => {
+    if (error.code) {
+      switch (error.code) {
         case "ERR_BAD_REQUEST":
         case "INVALID_REQUEST":
-          reject(new Error("잘못된 요청입니다."));
-          return;
         case "DUPLICATED_USER":
-          reject(new Error("중복된 이메일 혹은 닉네임이 존재합니다."));
-          return;
         case "AUTHENTICATION_FAILED":
-          reject(new Error("다시 로그인해주세요."));
-          return;
         case "ENTRY_NOT_FOUND":
-          reject(new Error("해당 항목 조회에 실패했습니다."));
-          return;
         case "CAPSULE_ALREADY_EXISTS":
-          reject(new Error("이미 캡슐이 존재합니다."));
+          reject(new AxiosError(error.response?.data.message));
           return;
       }
-      reject(new Error("알 수 없는 에러가 발생했습니다."));
     }
+    return new AxiosError("UNKNOWN_ERROR");
   });
 };
 
 export function initAxios() {
-  // TODO: axios 요청에 있어서 공통적으로 처리해야하는 config 추가 설정
-  // TODO: 서버에서 의도적으로 에러로 발생시켜 내려주는 경우 성공으로 처리하기
-  // axios.defaults.validateStatus = (status: number) => status === 0;
-
   axios.interceptors.request.use(requestOnFullFilled);
   axios.interceptors.response.use(responseOnFullFilled, responseOnRejected);
 }
